603e3f7632e07afad36f9b3d4226f89ec59ec372
feuskem <118988514+feuskem@users.noreply.github.com>
2024-01-13 17:23:36 +0300

    Temzileme+Birleştirme

--- VR__Archaeology/Saved/ShaderDebugInfo/PCD3D_SM6/MS_VoxelQuixelCollection/Default/FLocalVertexFactory/TBasePassPSFNoLightMapPolicySkylight/0/CompileDXC.bat
+++ VR__Archaeology/Saved/ShaderDebugInfo/PCD3D_SM6/MS_VoxelQuixelCollection/Default/FLocalVertexFactory/TBasePassPSFNoLightMapPolicySkylight/0/CompileDXC.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-SET DXC="C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+SET DXC="W:\UnrealEngine\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
 IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	ECHO Couldn't find dxc.exe under "W:\UnrealEngine\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
 	GOTO :END
 )
 %DXC% /auto-binding-space 0 /Zpr /O3 -Wno-parentheses-equality /T ps_6_6 /E MainPS /Fc BasePassPixelShader.d3dasm /Fo BasePassPixelShader.dxil BasePassPixelShader.usf
